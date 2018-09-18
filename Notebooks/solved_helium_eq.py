d71db94539a29c19b35d9bea4b3d0f96534a8d62
Daehyun You <daehyun.park.you@gmail.com>
2018-09-18 18:00:51 +0900

    Add notebook "Estimate intensity ratio r and volume effect h.ipynb" Add notebook "Estimate intensity ratio — its dependency of beta parameters pulse volume effect.ipynb" Update notebook "Estimate intensity ratio.ipynb" Add module solved_helium_eq.py and solved_helium_eq_with_volume_effect.py

---
+++ Notebooks/solved_helium_eq.py
@@ -0,0 +1,48 @@
+from functools import wraps
+
+from sympy import *
+
+__all__ = ['symargs', 'solved', 'lambdified']
+
+
+# %%
+coeff_p = Symbol('coeff_p')
+coeff_d = Symbol('coeff_d')
+coeff_s = Symbol('coeff_s')
+eta_d = Symbol('eta_d')
+eta_s = Symbol('eta_s')
+eta_p = Symbol('eta_p')
+
+solved = {
+    'beta1_amp':
+        (2 * coeff_p * sqrt(60 * coeff_d ** 2
+                            + 60 * sqrt(5) * coeff_d * coeff_s * cos(eta_d - eta_s)
+                            + 75 * coeff_s ** 2)
+         / (5 * (coeff_d ** 2 + coeff_p ** 2 + coeff_s ** 2))),
+    'beta1_shift':
+        arg(2 * sqrt(15) * coeff_d * cos(eta_d - eta_p) + 5 * sqrt(3) * coeff_s * cos(eta_p - eta_s)
+            + I * (2 * sqrt(15) * coeff_d * sin(eta_d - eta_p) - 5 * sqrt(3) * coeff_s * sin(eta_p - eta_s))),
+    'beta1m3_amp':
+        (2 * sqrt(3) * coeff_p * coeff_s
+         / (coeff_d ** 2 + coeff_p ** 2 + coeff_s ** 2)),
+    'beta1m3_shift': arg(-I * sin(eta_p - eta_s) + cos(eta_p - eta_s)),
+    'beta2':
+        ((10 * coeff_d ** 2 + 14 * sqrt(5) * coeff_d * coeff_s * cos(eta_d - eta_s) + 14 * coeff_p ** 2)
+         / (7 * (coeff_d ** 2 + coeff_p ** 2 + coeff_s ** 2))),
+    'beta3_amp':
+        (6 * sqrt(15) * coeff_d * coeff_p
+         / (5 * (coeff_d ** 2 + coeff_p ** 2 + coeff_s ** 2))),
+    'beta3_shift': arg(I * sin(eta_d - eta_p) + cos(eta_d - eta_p)),
+    'beta4': 18 * coeff_d ** 2 / (7 * (coeff_d ** 2 + coeff_p ** 2 + coeff_s ** 2)),
+}
+
+# %% lambdify pads
+symargs = {  # order sensitive!
+    s.name: s for s in [coeff_s, coeff_p, coeff_d, eta_s, eta_p, eta_d]
+}
+__lambdified = lambdify(tuple(symargs.values()), tuple(solved.values()), 'numpy')
+
+
+@wraps(__lambdified)
+def lambdified(*args, **kwargs):
+    return dict(zip(solved.keys(), __lambdified(*args, **kwargs)))
